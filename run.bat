e949e0d30ddbdd223ae15fe300322ae0307d4aa2
LiDing <72634327+linkedlist771@users.noreply.github.com>
2025-11-03 17:27:28 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @REM  .\python\Scripts\streamlit.exe run .\app.py
 
+
+@echo off
  .\python\python.exe .\gradio_app.py
+pause
+
+@REM .\python\Scripts\uv.exe pip install -e . --python .\python\python.exe
